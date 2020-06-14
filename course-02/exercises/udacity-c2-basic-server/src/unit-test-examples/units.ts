5b4739e3996cb3a917fa6b3fea95251add18f0c2
TravisFriday <Travisgifriday@gmail.com>
2020-06-14 03:41:21 -0700

    lesson 6 completed

--- course-02/exercises/udacity-c2-basic-server/src/unit-test-examples/units.ts
+++ course-02/exercises/udacity-c2-basic-server/src/unit-test-examples/units.ts
@@ -1,16 +1,27 @@
 // Super Simple Unit Functions
 
 export const add = (a: number, b: number) => {
-    return a + b;
-    }
+  return a + b;
+};
 
 export const divide = (a: number, b: number) => {
-    if(b === 0) { throw new Error('div by 0') }
+  if (b === 0) {
+    throw new Error("div by 0");
+  }
 
-    return a / b;
-    }
+  return a / b;
+};
 
 // @TODO try creating a method "concat" to concatenate two strings
+export const concat = (a: string, b: string) => {
+  if (a.length === 0) {
+    throw new Error("Your first String is Empty");
+  }
+  if (b.length === 0) {
+    throw new Error("Your second String is Empty");
+  }
+  return a + b;
+};
 // it should take two string paramaters.
 // it should return one string combining the two strings.
 // it should throw an error if either of the strings are empty.
